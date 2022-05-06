37e10eacab57aa581fc555e2752e9d05d80209c0
riderodd <joris.gaudin@gmail.com>
2022-05-06 15:22:21 +0200

    Added lib folder

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,40 @@
+import { Component, RefObject, SyntheticEvent } from 'react';
+import { ViewStyle, HostComponent } from 'react-native';
+declare type ArEvent = SyntheticEvent<{}, {
+    requestId: number | string;
+    result: string;
+    error: string;
+}>;
+declare type ArErrorEvent = SyntheticEvent<{}, {
+    message: string;
+}>;
+declare type ArViewerProps = {
+    model: string;
+    planeOrientation?: 'none' | 'vertical' | 'horizontal' | 'both';
+    allowScale?: boolean;
+    allowRotate?: boolean;
+    allowTranslate?: boolean;
+    lightEstimation?: boolean;
+    manageDepth?: boolean;
+    disableInstructions?: boolean;
+    style?: ViewStyle;
+    ref?: RefObject<HostComponent<ArViewerProps> | (() => never)>;
+    onDataReturned: (e: ArEvent) => void;
+    onError: (e: ArErrorEvent) => void | undefined;
+};
+declare type ArInnerViewProps = Omit<ArViewerProps, 'onDataReturned' | 'ref' | 'onError'>;
+export declare class ArViewerView extends Component<ArInnerViewProps> {
+    private _nextRequestId;
+    private _requestMap;
+    private nativeRef;
+    constructor(props: ArInnerViewProps);
+    _onDataReturned(event: ArEvent): void;
+    _onError(event: ArErrorEvent): void;
+    /**
+     * Takes a full screenshot of the rendered camera
+     * @returns A promise resolving a base64 encoded image
+     */
+    takeScreenshot(): Promise<string>;
+    render(): JSX.Element;
+}
+export {};
