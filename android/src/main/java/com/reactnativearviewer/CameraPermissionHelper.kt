088a19e38302f7135e113998bd933346ca853ad4
KingDCreations <mthais.web@gmail.com>
2022-10-19 10:00:11 +0200

    Updating react native to 0.70.2 on android

--- android/src/main/java/com/reactnativearviewer/CameraPermissionHelper.kt
+++ android/src/main/java/com/reactnativearviewer/CameraPermissionHelper.kt
@@ -2,10 +2,7 @@ package com.reactnativearviewer
 
 import android.Manifest
 import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
-import android.provider.Settings
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
